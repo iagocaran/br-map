64a65c659ef3ab8588c071d92cfdef62a9bb2312
Iago Caran Aquino <iago.caran@gmail.com>
2021-05-13 00:38:32 +0200

    Add debugging code

--- components/LeafletMap.vue
+++ components/LeafletMap.vue
@@ -12,9 +12,14 @@ export default {
   name: 'LeafletMap',
   methods: {
   },
+  fetch() {
+    console.log(process.env.NUXT_ENV_MAPBOX_API_KEY)
+  },
   mounted() {
     const L = require('leaflet');
     let mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+    console.log(process.env.NUXT_ENV_MAPBOX_API_KEY)
 
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
