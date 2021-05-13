95e00f7bd312ea9d066bd88dbed318082d7ea0c5
Iago Caran Aquino <iago.caran@gmail.com>
2021-05-14 00:28:30 +0200

    Fix missing .ts extension for modules entry on nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -45,7 +45,7 @@ export default {
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     '@nuxtjs/axios',
-    '~/modules/databaseSetup'
+    '~/modules/databaseSetup.ts'
   ],
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
